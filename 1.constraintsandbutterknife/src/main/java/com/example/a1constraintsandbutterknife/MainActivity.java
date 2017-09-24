71a4ee514b98a04313139d20ddfd16b30ae93454
MasterBlaster <14masterblaster14@gmail.com>
2017-09-24 18:24:23 +0530

    Successful Constraints layout and Butterknife inmplementation.

---
+++ 1.constraintsandbutterknife/src/main/java/com/example/a1constraintsandbutterknife/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.a1constraintsandbutterknife;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.center_textView)
+    TextView centerTextView;
+    @BindView(R.id.submit_button)
+    Button btn_submit;
+    @BindView(R.id.revert_button)
+    Button btn_revert;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        }); */
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick({R.id.submit_button, R.id.revert_button, R.id.fab})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+
+            case R.id.fab:
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                break;
+
+            case R.id.submit_button:
+                centerTextView.setText("Well Done Bro!!");
+                break;
+
+            case R.id.revert_button:
+                centerTextView.setText("Hello World!");
+                break;
+
+        }
+    }
+}
